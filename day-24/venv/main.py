0756373dfac46f4a6d2ecd81494789da4c5587e5
Chloe Howling <chloehltam@gmail.com>
2021-10-05 18:19:40 -0600

    Organized files

--- day-24/venv/main.py
+++
@@ -1,9 +0,0 @@
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
-
-with open("/Users/chloetam/PycharmProjects/day-24/venv/new_file.txt") as file:
-    content = file.read()
-    print(content)
-
-
